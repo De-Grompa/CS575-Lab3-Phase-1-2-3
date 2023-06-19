61f9f2c2f9fcdddf43e38b533f4b9e9d5ae06405
De-Grompa <jeremywalz99@gmail.com>
2023-06-18 20:06:06 -0500

    removed string[] from movie.cs
    changed it to string, new error occurred

--- Model/Movie.cs
+++ Model/Movie.cs
@@ -13,15 +13,15 @@ namespace MovieList.Model
         public int budget { get; set; }
         public int revenue { get; set; }
         public string? original_title { get; set; }
-        public string[]? cast { get; set; }
+        public string? cast { get; set; }
         public string? homepage { get; set; }
         public string? director { get; set; }
         public string? tagline { get; set; }
         public string? keywords { get; set; }
         public string? overview { get; set; }
         public int runtime { get; set; }
-        public string[]? genres { get; set; }
-        public string[]? production_companies { get; set; }
+        public string? genres { get; set; }
+        public string? production_companies { get; set; }
         public string? release_date { get; set; }
         public int vote_count { get; set; }
         public double vote_average { get; set; }
